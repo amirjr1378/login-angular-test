c39ec7d58096a1ce8b46dc6bd3cc0bfa261c062b
amir <jramir669@gmail.com>
2020-04-08 00:10:40 +0430

    delete extra line

--- src/app/layouts/private/private-routing.module.ts
+++ src/app/layouts/private/private-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
-import { AuthGuard } from 'src/app/guards/Auth';
 
 const routes: Routes = [
   {
